9b3fefc79793c8656d742aa927d9db5a7f344511
Amitava <dattaarup2004@gmail.com>
2024-11-04 22:04:16 +0530

    navbar done

--- linklynx/app/page.js
+++ linklynx/app/page.js
@@ -1,10 +1,11 @@
-import { Main } from "next/document";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      I am main
-    </main>
+    <div>
+      <main>
+        I am home
+      </main>
+    </div>
   );
 }
